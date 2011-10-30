654ca31bfb889390ba3f957f28fe557988e370a2
Tobias Rumrich <github@tobiasrumrich.de>
2011-10-30 02:29:05 +0200

    Abstraft Person entfernt

--- ExaminationManager/src/de/hatoma/exman/model/person/Student.java
+++ ExaminationManager/src/de/hatoma/exman/model/person/Student.java
@@ -6,63 +6,72 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.envers.AuditTable;
 import org.hibernate.envers.Audited;
-import de.hatoma.exman.model.study.Maniple;
-import javax.persistence.ManyToOne;
-import de.hatoma.exman.model.study.Century;
 
+import de.hatoma.exman.model.study.Century;
+import de.hatoma.exman.model.study.Maniple;
 
 @Entity
 @Audited
-@Table(name = "Student")
-@AuditTable(value = "ADT_Student")
-public class Student extends AbstractPerson implements Serializable {
+@Table(name = "Students")
+@AuditTable(value = "Student_Revisions")
+public class Student implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
 	private long id;
+	private String forename;
+	private String lastname;
 
-	private Maniple maniple;
+	public String getForename() {
+		return forename;
+	}
 
-	private Century century;
+	public void setForename(String forename) {
+		this.forename = forename;
+	}
 
-	/**
-	 * @return the id
-	 */
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	public long getId() {
 		return id;
 	}
 
-	/**
-	 * @param id the id to set
-	 */
 	public void setId(long id) {
 		this.id = id;
 	}
 
+	private Maniple maniple;
+
+	private Century century;
+
 	@ManyToOne
 	public Maniple getManiple() {
-	    return maniple;
+		return maniple;
 	}
 
 	public void setManiple(Maniple param) {
-	    this.maniple = param;
+		this.maniple = param;
 	}
 
 	@ManyToOne
 	public Century getCentury() {
-	    return century;
+		return century;
 	}
 
 	public void setCentury(Century param) {
-	    this.century = param;
+		this.century = param;
 	}
-	
-	
 
 }
