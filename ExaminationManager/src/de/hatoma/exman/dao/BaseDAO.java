9dcf55a3bb744bc1f22b467aac9e6e82524e935f
Hannes Lemberg <lemberg.hannes+aa@gmail.com>
2011-11-12 15:35:37 +0100

    baseDAO wieder hergestellt :D

---
+++ ExaminationManager/src/de/hatoma/exman/dao/BaseDAO.java
@@ -0,0 +1,41 @@
+package de.hatoma.exman.dao;
+
+import java.util.List;
+
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+public abstract class BaseDAO<T> extends HibernateDaoSupport implements IDAO<T> {
+	private Class<T> clazz;
+
+	public BaseDAO(Class<T> clazz) {
+		this.clazz = clazz;
+	}
+
+	/** {@inheritDoc} */
+	@SuppressWarnings("unchecked")
+	@Override
+	public T save(T t) {
+		return (T) getHibernateTemplate().save(t);
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public void delete(T t) {
+		getHibernateTemplate().delete(t);
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	@SuppressWarnings("unchecked")
+	public List<T> findAll() {
+		return getHibernateTemplate().loadAll(clazz.getClass());
+	}
+
+	/** {@inheritDoc} */
+	@SuppressWarnings("unchecked")
+	@Override
+	public T load(long id) {
+
+		return (T) getHibernateTemplate().get(clazz.getClass(), id);
+	}
+}
