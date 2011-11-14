5b42d2a90db60af34c28805a32300427e18e4a71
Tobias Rumrich <github@tobiasrumrich.de>
2011-11-14 14:53:21 +0100

    DAO Interfaces umbenannt (case-sensitivity-issue with Mac)

--- ExaminationManager/src/de/hatoma/exman/dao/IStudentDAO.java
+++
@@ -1,7 +0,0 @@
-package de.hatoma.exman.dao;
-
-import de.hatoma.exman.model.Student;
-
-public interface IStudentDao extends IDao<Student> {
-
-}
--- ExaminationManager/src/de/hatoma/exman/dao/IStudyBranchDAO.java
+++
@@ -1,7 +0,0 @@
-package de.hatoma.exman.dao;
-
-import de.hatoma.exman.model.StudyBranch;
-
-public interface IStudyBranchDao extends IDao<StudyBranch> {
-
-}
