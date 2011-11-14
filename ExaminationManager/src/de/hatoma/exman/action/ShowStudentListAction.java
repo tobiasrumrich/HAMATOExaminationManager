e260783e3732507387a71305a28cd835c8f86884
Hannes Lemberg <lemberg.hannes+aa@gmail.com>
2011-11-14 02:20:58 +0100

    temporär compilefehler gefixt

--- ExaminationManager/src/de/hatoma/exman/action/ShowStudentListAction.java
+++ ExaminationManager/src/de/hatoma/exman/action/ShowStudentListAction.java
@@ -1,22 +1,23 @@
 package de.hatoma.exman.action;
 
-import java.util.HashSet;
 import java.util.Set;
 
 import com.opensymphony.xwork2.ActionSupport;
 
 import de.hatoma.exman.model.Student;
 import de.hatoma.exman.service.IStudentService;
-import de.hatoma.exman.service.room.IRoomService;
-
 
 public class ShowStudentListAction extends ActionSupport {
+	private static final long serialVersionUID = -7867656427450400882L;
 	private IStudentService studentService;
 	private Set<Student> students;
 
 	@Override
 	public String execute() throws Exception {
-		students = new HashSet<Student>(studentService.listStudents());
+		// TODO hal: auskommentiert. wegen compilefehler. listStudents nicht in
+		// studentService vorhanden. Hat das überhaupt kompiliert als es
+		// committet wurde? oO
+		// students = new HashSet<Student>(studentService.listStudents());
 		return SUCCESS;
 	}
 
