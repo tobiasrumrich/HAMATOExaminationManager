4b30050cad42ea0d37e88db8aecc88df46aff569
Hannes Lemberg <lemberg.hannes+aa@gmail.com>
2011-11-14 02:23:42 +0100

    Annotationsbasiert

--- ExaminationManager/src/de/hatoma/exman/dao/ExamDAO.java
+++ ExaminationManager/src/de/hatoma/exman/dao/ExamDAO.java
@@ -1,13 +1,15 @@
 package de.hatoma.exman.dao;
 
+import org.springframework.stereotype.Component;
+
 import de.hatoma.exman.model.Exam;
 
+@Component
 public class ExamDAO extends BaseDAO<Exam> implements IExamDAO {
 
 	public ExamDAO() {
 		super(Exam.class);
 		// TODO Auto-generated constructor stub
 	}
-
 
 }
