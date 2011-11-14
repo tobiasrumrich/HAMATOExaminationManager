9b32e8205138fb5896e8cff5fda0102ca51ae0e2
Hannes Lemberg <lemberg.hannes+aa@gmail.com>
2011-11-14 02:23:08 +0100

    BaseDao wird verwendet

--- ExaminationManager/src/de/hatoma/exman/dao/ExamAttendanceDAO.java
+++ ExaminationManager/src/de/hatoma/exman/dao/ExamAttendanceDAO.java
@@ -1,32 +1,14 @@
 package de.hatoma.exman.dao;
 
-import java.util.List;
-
-import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import org.springframework.stereotype.Component;
 
 import de.hatoma.exman.model.ExamAttendance;
 
-public class ExamAttendanceDAO extends HibernateDaoSupport implements IExamAttendanceDAO {
+@Component
+public class ExamAttendanceDAO extends BaseDAO<ExamAttendance> implements
+		IExamAttendanceDAO {
 
-	@Override
-	public ExamAttendance save(ExamAttendance entity) {
-		getHibernateTemplate().save(entity);
-		return entity;
+	public ExamAttendanceDAO() {
+		super(ExamAttendance.class);
 	}
-
-	@Override
-	public void delete(ExamAttendance entity) {
-		getHibernateTemplate().delete(entity);
-	}
-
-	@Override
-	public List<ExamAttendance> findAll() {
-		return getHibernateTemplate().loadAll(ExamAttendance.class);
-	}
-
-	@Override
-	public ExamAttendance load(long id) {
-		return (ExamAttendance) getHibernateTemplate().get(ExamAttendance.class, id);
-	}
-
 }
