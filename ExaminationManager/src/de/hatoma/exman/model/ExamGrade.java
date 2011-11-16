c2719ed537da046e285021e082180e328a1cba5a
Tobias Rumrich <github@tobiasrumrich.de>
2011-11-16 03:01:01 +0100

    ExamGrade kennt nun auch 2.3 und 2.7

--- ExaminationManager/src/de/hatoma/exman/model/ExamGrade.java
+++ ExaminationManager/src/de/hatoma/exman/model/ExamGrade.java
@@ -3,7 +3,7 @@ package de.hatoma.exman.model;
 import java.io.Serializable;
 
 public enum ExamGrade implements Serializable {
-	G10(10,"1.0"), G13(13,"1.3"), G17(17,"1.7"), G20(20,"2.0"), G30(30,"3.0"), G33(33,"3.3"), G37(37,"3.7"), G40(40,"3.7"), G50(50,"5.0"), G60(60,"6.0");
+	G10(10,"1.0"), G13(13,"1.3"), G17(17,"1.7"), G20(20,"2.0"), G23(20,"2.3"), G27(27,"2.3"), G30(30,"3.0"), G33(33,"3.3"), G37(37,"3.7"), G40(40,"3.7"), G50(50,"5.0"), G60(60,"6.0");
 
 	private int asNumber;
 	private String asExpression;
