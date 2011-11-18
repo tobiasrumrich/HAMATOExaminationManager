cb052f450747b32e47ee54f142cd6acbb1c01613
Tobias Rumrich <github@tobiasrumrich.de>
2011-11-18 18:19:01 +0100

    Room Verweis aus AuditTrail Action Beispiel entfernt

--- ExaminationManager/src/de/hatoma/exman/action/AuditTrailAction.java
+++ ExaminationManager/src/de/hatoma/exman/action/AuditTrailAction.java
@@ -1,10 +1,6 @@
 package de.hatoma.exman.action;
 
-import java.util.HashSet;
-
 import com.opensymphony.xwork2.Action;
-
-import de.hatoma.exman.model.room.Room;
 
 public class AuditTrailAction implements Action {
 
