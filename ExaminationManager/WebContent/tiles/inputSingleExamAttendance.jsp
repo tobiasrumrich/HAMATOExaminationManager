9f4f22ce77c1abeb7a1330955d722a4a42e5f288
Hannes Lemberg <lemberg.hannes+aa@gmail.com>
2011-11-20 00:46:41 +0100

    Weitere anpassungen SIngelExamAttendance

--- ExaminationManager/WebContent/tiles/inputSingleExamAttendance.jsp
+++ ExaminationManager/WebContent/tiles/inputSingleExamAttendance.jsp
@@ -1,17 +1,41 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <%@ taglib uri="/struts-tags" prefix="s"%>
+<s:url action="FileSingleExamAttendance" method="getExamsForStudent"
+	var="ajaxGetUrl">
+	<s:param name="studentId" value="-1" />
+</s:url>
 
+<script>
+	var allStudents = <s:property value="allStudents" escape="false"/>;
+	var genericUrl = "<s:property value='ajaxGetUrl' />";
+
+	$(function() {
+		hamatoAutocomplete($("#studentI"), $("#studentIid"), allStudents);
+		$("#studentIid").change(function() {
+			$('#examI').find('option').remove();
+			$.ajax({
+				url : genericUrl.replace("-1", $("#studentIid").val()),
+				context : document.body,
+				success : function() {
+					$(this).addClass("done");
+				}
+			});
+		});
+
+	});
+</script>
+<s:debug />
 <s:form validate="true">
 	<s:token />
 	<s:textfield name="studentN" id="studentI" key="lblStudentName"
 		required="true" />
 	<s:hidden name="studentNid" id="studentIid" />
-	<s:textfield name="examSubject" key="lblExamSubject" required="true" />
-	<s:select name="exam" key="lblExam" list="{}" disabled="true"
-		required="true" />
+	<s:select name="examN" id="examI" key="lblExam" list="{}"
+		disabled="true" required="true" />
 	<s:select key="lblExamGrade" list="allGrades" required="true" />
 
 	<s:submit key="btnSave" action="FileSingleExamAttendance" method="save" />
 	<s:submit key="btnCancel" action="SaveNewExam" name="btnCancel" />
 </s:form>
+
--- ExaminationManager/src/de/hatoma/exman/action/SingleExamAttendanceAction.java
+++ ExaminationManager/src/de/hatoma/exman/action/SingleExamAttendanceAction.java
@@ -3,10 +3,13 @@ package de.hatoma.exman.action;
 import java.util.Map;
 import java.util.TreeMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
 
 import de.hatoma.exman.model.ExamGrade;
+import de.hatoma.exman.service.IStudentService;
 
 /**
  * @author Hannes Lemberg 3547
@@ -14,14 +17,12 @@ import de.hatoma.exman.model.ExamGrade;
  */
 public class SingleExamAttendanceAction extends ActionSupport {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-
-	public String input() {
-		return Action.INPUT;
-	}
+	private String studentN;
+	private Long studentNid;
+	@Autowired
+	private IStudentService studentService;
+	private Long studentId;
 
 	public Map<String, String> getAllGrades() {
 		TreeMap<String, String> grades = new TreeMap<String, String>();
@@ -33,5 +34,49 @@ public class SingleExamAttendanceAction extends ActionSupport {
 		}
 
 		return grades;
+	}
+
+	public String getExamsForStudent() {
+		return null;
+	}
+
+	public String getAllStudents() {
+		return studentService.getAllStudentsAsJson();
+	}
+
+	public String getStudentN() {
+		return studentN;
+	}
+
+	public Long getStudentNid() {
+		return studentNid;
+	}
+
+	public IStudentService getStudentService() {
+		return studentService;
+	}
+
+	public String input() {
+		return Action.INPUT;
+	}
+
+	public void setStudentN(String studentN) {
+		this.studentN = studentN;
+	}
+
+	public void setStudentNid(Long studentNid) {
+		this.studentNid = studentNid;
+	}
+
+	public void setStudentService(IStudentService studentService) {
+		this.studentService = studentService;
+	}
+
+	public Long getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(Long studentId) {
+		this.studentId = studentId;
 	}
 }
